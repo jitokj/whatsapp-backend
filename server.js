1af206604ce5ce5011c7b21281e12d8046fd294e
jito <jitokj3@gmail.com>
2020-11-01 22:20:39 +0530

    added schema

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Cors = require('cors');
+const Message = require("./dbMessages");
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -12,7 +13,29 @@ const Connection_url = process.env.API_URL;
 app.use(express.json());
 app.use(Cors());
 
+app.post("/messages/new",(req,res)=>{
+    const dbMessage = req.body;
+    Message.create(dbMessage,(err,data)=>{
+        if(err)
+        {
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(`new messages created:\n ${data}`);
+        }
+    })
+})
 
+app.get('/messages/sync',(req,res)=>{
+    Message.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else {
+            res.status(200).send(data);
+        }
+    })
+})
 app.get('/',(req,res)=>{
     res.status(200).send('hello world');
 });
