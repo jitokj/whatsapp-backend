93ea13914e019c5452b1c4aa7aeb8eb9731ebff2
jito <jitokj3@gmail.com>
2020-11-02 12:04:31 +0530

    changed pusher message structure

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ db.once("open",()=>{
                name: messageDetails.name,
                message: messageDetails.message,
                timestamp: messageDetails.timestamp,
+               received :messageDetails.received
            })
           
        }
